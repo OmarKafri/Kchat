4b68efd7dda3b560bc06f7f589521f3df87d5e81
OmarKafri <omarkafri@gmail.com>
2025-11-24 13:03:38 +0400

    fix the backend url for socket file

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,4 +1,5 @@
-
 import { io, Socket } from "socket.io-client";
 
-export const socket: Socket = io("http://localhost:4000");
+export const socket: Socket = io(
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000"
+);
