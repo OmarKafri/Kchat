1519e10bae3a2bfd868c939974e01a6d0f76736f
OmarKafri <omarkafri@gmail.com>
2025-11-24 13:39:25 +0400

    fix socket again

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,9 @@
 import { io, Socket } from "socket.io-client";
 
 export const socket: Socket = io(
-  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000"
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000",
+  {
+    transports: ["websocket"],
+    secure: true,
+  }
 );
