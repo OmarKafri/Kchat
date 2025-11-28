b9f67487ec29fa3fe6ed3a232325c977898d788f
OmarKafri <omarkafri@gmail.com>
2025-11-28 11:42:47 +0400

    fixing middleware for login again

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import { auth } from "@/auth";
 
-export default auth((req) => {
-  const { pathname } = req.nextUrl;
-  const session = req.auth;
+export async function middleware(req: Request) {
+  const session = await auth();
+  const { pathname } = new URL(req.url);
 
   if (session && (pathname === "/login" || pathname === "/register")) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
@@ -14,7 +14,7 @@ export default auth((req) => {
   }
 
   return NextResponse.next();
-});
+}
 
 export const config = {
   matcher: ["/dashboard/:path*", "/login", "/register"],
